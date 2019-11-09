6d7f7597a38471b93b88d2a57d4b483d435a971d
crockb <crockb@oregonstate.edu>
2019-11-09 17:02:16 -0600

    Updated makefile to reference unittest_helpers - retry #6

--- projects/crockb/dominion/unittest_helpers.h
+++ projects/crockb/dominion/unittest_helpers.h
@@ -6,7 +6,6 @@
 #include "rngs.h"
 
 // helper functions
-int testPlayBaron();
 int assert(int expected, int actual);
 int hasGameCard(int card, struct gameState *state, int pileToCheck);
 int countCardTypeInHand(int card, struct gameState *state);
