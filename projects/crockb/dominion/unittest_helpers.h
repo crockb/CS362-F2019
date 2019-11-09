4a95bc4fe2b6ecb6cd1117f64af08f60f794bf6e
crockb <crockb@oregonstate.edu>
2019-11-09 17:37:50 -0600

    Updated makefile to reference unittest_helpers - retry #15

--- projects/crockb/dominion/unittest_helpers.h
+++ projects/crockb/dominion/unittest_helpers.h
@@ -2,6 +2,7 @@
 #define _UNITTEST_HELPERS_H
 
 #include "dominion.h"
+#include "dominion_helpers.h"
 #include "rngs.h"
 
 // helper functions
@@ -14,7 +15,7 @@ int removeEstateCardFromHand(int player, struct gameState *state);
 
 // helper print functions
 void printUnitTest1Condition1Results(struct gameState *state, struct gameState *preState);
-void printUnitTest1Condition2to4Results(struct gameState *state, struct gameState *preState)
+void printUnitTest1Condition2to4Results(struct gameState *state, struct gameState *preState);
 void printPlayersCards(int player, struct gameState *state);
 void printAllSupplyCounts(struct gameState *state);
 void printAllGameStateVariables(struct gameState *state);
