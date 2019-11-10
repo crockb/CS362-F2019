3b1969c7d3e147753bbed75cc8ac567600482241
crockb <crockb@oregonstate.edu>
2019-11-09 18:15:59 -0600

    Reverted back to before makefile - #1

--- projects/crockb/dominion/unittest1.c
+++ projects/crockb/dominion/unittest1.c
@@ -13,8 +13,6 @@
 		CONDITION #4:  Gain an Estate (1 Estate Available):  +1 hand[estate], -1 hand[baron], 5 handCount, same coins, +1 buy, 0 actions, +1 discard[baron], -1 supply[estate]
 
 
-
-
    	Known Bugs Inserted in Assignment 2:
 		1. Omitted a necessary step(s) to “discardCard” when the estate card was found.  
            This will cause the player’s hand to incorrectly retain an estate during gameplay.
@@ -31,6 +29,8 @@
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include "rngs.h"
+
+
 
 
 // helper function signatures
