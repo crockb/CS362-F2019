e003d7b708c41ab36483153c9c243ccc1dedd4b5
crockb <crockb@oregonstate.edu>
2019-11-09 20:29:15 -0600

    TestCondition 1, precondition 3

--- projects/crockb/dominion/unittest2.c
+++ projects/crockb/dominion/unittest2.c
@@ -95,6 +95,22 @@ int testPlayMinion()
 
     // check the results
     printTestCondition2Results(&state, &preState);
+    
+    // print player's cards
+    printf("player 1 (pre-state)\n");
+    printPlayersCards(0, &preState);
+    printf("player 1 (post-state)\n");
+    printPlayersCards(0, &state);
+
+    printf("player 2 (pre-state)\n");
+    printPlayersCards(1, &preState);
+    printf("player 2 (post-state)\n");
+    printPlayersCards(1, &state);
+
+    printf("player 3 (pre-state)\n");
+    printPlayersCards(2, &preState);
+    printf("player 3 (post-state)\n");
+    printPlayersCards(2, &state);
 
 
 /*
