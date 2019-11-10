3eb8bd2d906945e6ce4f93b5d42ccdc81f94cbb7
crockb <crockb@oregonstate.edu>
2019-11-09 22:08:59 -0600

    Test condition #1-3 - retry#3

--- projects/crockb/dominion/unittest2.c
+++ projects/crockb/dominion/unittest2.c
@@ -34,8 +34,6 @@ void printTestCondition2Results(struct gameState *state, struct gameState *preSt
 void printTestCondition3Results(struct gameState *state, struct gameState *preState);
 
 void printPlayersCards(int player, struct gameState *state);
-void printAllSupplyCounts(struct gameState *state);
-void printAllGameStateVariables(struct gameState *state);
 
 // call unittest2
 int main()
@@ -280,81 +278,4 @@ void printPlayersCards(int player, struct gameState *state)
   printf("Player's deck:\n");
   for (i = 0; i < state->deckCount[player]; i++)
     printf("  Card #%d: %d\n", i+1, state->deck[player][i]);
-}
-
-
-void printAllSupplyCounts(struct gameState *state)
-{
-  printf("supplyCount (curse): %d\n", state->supplyCount[curse]);
-  printf("supplyCount (estate): %d\n", state->supplyCount[estate]);
-  printf("supplyCount (duchy): %d\n", state->supplyCount[duchy]);
-  printf("supplyCount (province): %d\n", state->supplyCount[province]);
-  printf("supplyCount (copper): %d\n", state->supplyCount[copper]);
-  printf("supplyCount (silver): %d\n", state->supplyCount[silver]);
-  printf("supplyCount (gold): %d\n", state->supplyCount[gold]);
-  printf("supplyCount (baron): %d\n", state->supplyCount[baron]);
-  printf("supplyCount (gardens): %d\n", state->supplyCount[gardens]);
-  printf("supplyCount (ambassador): %d\n", state->supplyCount[ambassador]);
-  printf("supplyCount (village): %d\n", state->supplyCount[village]);
-  printf("supplyCount (minion): %d\n", state->supplyCount[minion]);
-  printf("supplyCount (mine): %d\n", state->supplyCount[mine]);
-  printf("supplyCount (sea_hag): %d\n", state->supplyCount[sea_hag]);
-  printf("supplyCount (tribute): %d\n", state->supplyCount[tribute]);
-  printf("supplyCount (smithy): %d\n", state->supplyCount[smithy]);
-
-}
-
-
-void printAllGameStateVariables(struct gameState *state)
-{
-
-  int i;
-
-    // print out the results of the game state
-  printf("numPlayers: %d\n", state->numPlayers);
-  printf("supplyCount (curse): %d\n", state->supplyCount[curse]);
-  printf("supplyCount (estate): %d\n", state->supplyCount[estate]);
-  printf("supplyCount (duchy): %d\n", state->supplyCount[duchy]);
-  printf("supplyCount (province): %d\n", state->supplyCount[province]);
-  printf("supplyCount (copper): %d\n", state->supplyCount[copper]);
-  printf("supplyCount (silver): %d\n", state->supplyCount[silver]);
-  printf("supplyCount (gold): %d\n", state->supplyCount[gold]);
-  printf("supplyCount (baron): %d\n", state->supplyCount[baron]);
-  printf("supplyCount (gardens): %d\n", state->supplyCount[gardens]);
-  printf("supplyCount (ambassador): %d\n", state->supplyCount[ambassador]);
-  printf("supplyCount (village): %d\n", state->supplyCount[village]);
-  printf("supplyCount (minion): %d\n", state->supplyCount[minion]);
-  printf("supplyCount (mine): %d\n", state->supplyCount[mine]);
-  printf("supplyCount (sea_hag): %d\n", state->supplyCount[sea_hag]);
-  printf("supplyCount (tribute): %d\n", state->supplyCount[tribute]);
-  printf("supplyCount (smithy): %d\n", state->supplyCount[smithy]);
-  printf("whoseTurn (1): %d\n", state->whoseTurn);
-  printf("phase: %d\n", state->phase);
-  printf("numActions: %d\n", state->numActions);
-  printf("numBuys: %d\n", state->numBuys);
-
-  printf("Player 1's hand:\n");
-  for (i = 0; i < state->handCount[0]; i++)
-    printf("  Card #%d: %d\n", i+1, state->hand[0][i]);
-
-  printf("Player 2's hand:\n");
-  for (i = 0; i < state->handCount[1]; i++)
-    printf("  Card #%d: %d\n", i+1, state->hand[1][i]);
-
-  printf("Player 1's deck:\n");
-  for (i = 0; i < state->deckCount[0]; i++)
-    printf("  Card #%d: %d\n", i+1, state->deck[0][i]);
-
-  printf("Player 2's deck:\n");
-  for (i = 0; i < state->deckCount[1]; i++)
-    printf("  Card #%d: %d\n", i+1, state->deck[1][i]);
-
-  printf("Player 1's discards:\n");
-  for (i = 0; i < state->discardCount[0]; i++)
-    printf("  Card #%d: %d\n", i+1, state->discard[0][i]);
-
-  printf("Player 2's discards:\n");
-  for (i = 0; i < state->discardCount[1]; i++)
-    printf("  Card #%d: %d\n", i+1, state->discard[1][i]);
-
 }
