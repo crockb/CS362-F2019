eba2f00678b7be711c204ceda94b3bf27d08c32c
crockb <crockb@oregonstate.edu>
2019-11-16 18:47:17 -0600

    Checking game states

--- projects/crockb/dominion/randomtestcard1.c
+++ projects/crockb/dominion/randomtestcard1.c
@@ -95,7 +95,7 @@ int main()
 int testPlayBaron()
 {
 
-    //int player1 = 0;
+    int n;
     struct gameState state;
     int k[10] = {baron, gardens, ambassador, village, minion, mine, cutpurse,
                sea_hag, tribute, smithy
