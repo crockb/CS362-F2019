ff0f882b6366c8781f516f5b82b38554fc5b701e
crockb <crockb@oregonstate.edu>
2019-11-17 12:19:32 -0600

    Testing playTribute() setup

--- projects/crockb/dominion/randomtestcard3.c
+++ projects/crockb/dominion/randomtestcard3.c
@@ -2,7 +2,7 @@
 	Author/Date:  Bryant Crock  11/16/19
 	Description:  Test the outcomes of the refactored tribute card playTribute();
 	Business Requirements for Tribute:
-		 1. The player to the left reveals then discards top 2 cards
+		 1. The player to the left reveals then discards top 2 deck cards
 		 2. For each differently named card:
 		 		- Action: +2 actions
 		 		- Treasure Card: +2 coins
@@ -37,3 +37,115 @@
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include "rngs.h"
+
+
+// helper function signatures
+int testPlayTribute();
+
+
+// call randomtestcard2
+int main()
+{
+    srand(time(NULL));
+    testPlayTribute();
+    return 0;
+}
+
+
+// function to execute the set of conditions for unittest#1
+int testPlayTribute()
+{
+	printf("Hello friend.\n");
+	return 0;
+}
+
+
+
+
+/*
+
+int playTribute(struct gameState *state, int handPos)
+{
+    int i;
+    int currentPlayer = whoseTurn(state);
+    int nextPlayer = currentPlayer + 1;
+    int tributeRevealedCards[2] = {-1, -1};
+
+    // check to see if the next player is player 0
+    if (nextPlayer > (state->numPlayers - 1)) {
+        nextPlayer = 0;
+    }
+
+    // discard tribute card
+    discardCard(handPos, currentPlayer, state, 0);
+
+    // check the availability of nextplayer cards and set them to the tribute cards
+        if ((state->discardCount[nextPlayer] + state->deckCount[nextPlayer]) <= 1) {
+            if (state->deckCount[nextPlayer] > 0) {
+                tributeRevealedCards[0] = state->deck[nextPlayer][state->deckCount[nextPlayer]-1];
+                discardCard(state->deckCount[nextPlayer]-1, nextPlayer, state, 0);
+                state->deckCount[nextPlayer]--;
+            }
+            else if (state->discardCount[nextPlayer] > 0) {
+                tributeRevealedCards[0] = state->discard[nextPlayer][state->discardCount[nextPlayer]-1];
+            }
+            else {
+                //No Card to Reveal
+                if (DEBUG) {
+                    printf("No cards to reveal\n");
+                }
+            }
+        }
+
+        else {
+            if (state->deckCount[nextPlayer] == 0) {
+                for (i = 0; i < state->discardCount[nextPlayer]; i++) {
+                    state->deck[nextPlayer][i] = state->discard[nextPlayer][i];//Move to deck
+                    state->deckCount[nextPlayer]++;
+                    state->discard[nextPlayer][i] = -1;
+                    state->discardCount[nextPlayer]--;
+                }
+
+                shuffle(nextPlayer,state);//Shuffle the deck
+            }
+            tributeRevealedCards[0] = state->deck[nextPlayer][state->deckCount[nextPlayer]-1];
+            discardCard(state->deckCount[nextPlayer], nextPlayer, state, 0);
+
+            tributeRevealedCards[1] = state->deck[nextPlayer][state->deckCount[nextPlayer]-1];
+            discardCard(state->deckCount[nextPlayer], nextPlayer, state, 0);
+        }
+
+        if (tributeRevealedCards[0] == tributeRevealedCards[1]) { //If we have a duplicate card, just drop one
+            state->playedCards[state->playedCardCount] = tributeRevealedCards[1];
+            state->playedCardCount++;
+        }
+
+        for (i = 0; i < 2; i ++) {
+            if (tributeRevealedCards[i] == copper || tributeRevealedCards[i] == silver || tributeRevealedCards[i] == gold) { //Treasure cards
+                state->coins += 2;
+            }
+
+            else if (tributeRevealedCards[i] == estate || tributeRevealedCards[i] == duchy || tributeRevealedCards[i] == province || tributeRevealedCards[i] == gardens || tributeRevealedCards[i] == great_hall) { //Victory Card Found
+                drawCard(currentPlayer, state);
+                drawCard(currentPlayer, state);
+
+                // great hall receives bonus from Action as well
+                if (tributeRevealedCards[i] == great_hall) {
+                    state->numActions = state->numActions + 2;
+                }
+            }
+
+            else if (tributeRevealedCards[i] == -1)
+            {
+                // do nothing - invalid card
+            }
+
+            else { //Action Card
+                state->numActions = state->numActions + 2;
+            }
+        }
+
+        return 0;
+}
+
+*/
