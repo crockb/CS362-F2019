4cbaf5dd130ab8095ffb9660f8f82258d336e584
crockb <crockb@oregonstate.edu>
2019-12-02 22:52:38 -0600

    Updated files

--- projects/FinalProject-Bugs/dominion/unittest-bug8.c
+++ projects/FinalProject-Bugs/dominion/unittest-bug8.c
@@ -1,17 +1,17 @@
 /* unittest-bug8.c
-	Author/Date:  Robert Pino     12/1/19
-	Description:  Test whether the Baron, Minion, and Tribute cards correctly update their bonus coins. 
-	
-	Business Requirements for the cards:
-		 1. player1's coins are set to 0.
+  Author/Date:  Robert Pino     12/1/19
+  Description:  Test whether the Baron, Minion, and Tribute cards correctly update their bonus coins. 
+  
+  Business Requirements for the cards:
+     1. player1's coins are set to 0.
      2. Minion card is played.
         A. Minion card is the easiest to implement in the test.
         B. Also, the bug we are testing for is the exact same code in all three cards, so if the bug can be isolated in one card, we can be sure it is the same bug in the others. 
         C. Minion card SHOULD grant player1 2 bonus coins.
      2. We test whether player1's coins are equal to 2.
- 		    
- 	Expected Unittest Outcomes:
- 			player1's coins: 2
+        
+  Expected Unittest Outcomes:
+      player1's coins: 2
 */
 
 #include "dominion.h"
@@ -27,36 +27,42 @@
 // helper function signatures
 int setState();
 int runTest(struct gameState *state, struct gameState *preState);
-int newInitialize(int numPlayers, int kingdomCards[10], int randomSeed,struct gameState *state);
+void printPlayersCards(int player, struct gameState *state);
 
 int main()
 {
 
-	  setState();
-	  return 0;
+    setState();
+    return 0;
 }
 
 int setState()
 {
 // set card array - all Minion cards so they are the only card played
-  int k[10] = { minion, minion, minion, minion, minion, minion, minion, tribute, minion, minion };
-  int bonus = 0;
+    int k[10] = {baron, gardens, ambassador, village, minion, mine, cutpurse,
+               sea_hag, tribute, smithy
+            };
+  int gameSeed = 1234;
+
 //declare the game state
   struct gameState G, preState;
 
-// initialize a new game
-  int gameSeed = 1234;
+  printf("\n----- UNIT TEST - Bug#8 - The number of bonus coins from actions does not appear to be recorded correctly in cardEffect. ");
 
   //(this is a new, shortened function that removes any code that might grant coins)
   initializeGame(2, k, gameSeed, &G);
-  updateCoins(0,&G,bonus);
+
+  
+  // provide player1 with a minion card
+
+  G.hand[0][0] = minion;
+  G.supplyCount[minion]--;
+
+  updateCoins(0, &G, 0);
   memcpy(&preState, &G, sizeof(struct gameState));
 
-  printf("\n----- UNIT TEST - Bug#8 - The number of bonus coins from actions does not appear to be recorded correctly in cardEffect. ");
-
 //player1 plays a card (guaranteed to be a Minion) with choice 1 (+2 coin)
-  //int playCard(int handPos, int choice1, int choice2, int choice3, struct gameState *state)
-  playCard(0, 1, 1, 1, &G);
+  playCard(0, 1, 0, 0, &G);
 
 //run the test
   runTest(&G, &preState);
@@ -81,5 +87,19 @@ int runTest(struct gameState *state, struct gameState *preState)
         printf("\nCoins SUCCESSFULLY: actual = %d, expected = %d\n", actual, expected);
         return 0;
       };
-	return 0;
+  return 0;
+}
+
+
+void printPlayersCards(int player, struct gameState *state)
+{
+    int i;
+
+    // print the hand
+   printf("Player's hand:\n");
+   for (i = 0; i < state->handCount[player]; i++)
+    printf("  Card #%d: %d\n", i+1, state->hand[player][i]);
+
+  printf("state->coins = %d\n", state->coins);
+
 }
